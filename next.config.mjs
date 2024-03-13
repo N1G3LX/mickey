76f2dbcf142e0e781bb295d8b52a02a7b0167a49
Adewale shittu <sheriffhardewale@gmail.com>
2024-03-13 19:47:02 +0100

    disabled ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+  },};
 
 export default nextConfig;
