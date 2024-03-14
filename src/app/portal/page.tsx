9964489e02868d3d38f7752a7e8d59428ff0211d
Adewale shittu <sheriffhardewale@gmail.com>
2024-03-14 11:34:09 +0100

    fixed ls

--- src/app/portal/page.tsx
+++ src/app/portal/page.tsx
@@ -1,18 +1,20 @@
 'use client'
-import Footer from "@/components/landing-page/Footer";
 import Header from "@/components/landing-page/Header";
-import useSWR from "swr";
+
 import TableData from "@/components/landing-page/TableData";
 import TokenInfo from "@/components/landing-page/TokenInfo";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Portal = () => {
   const {isAuthenticated} = useAuthContext()
   const router = useRouter()
+ useEffect(() =>{
   if(isAuthenticated === false){
     return router.push('/')
   }
+ })
   return (
     <main className="flex flex-col">
     <Header />
