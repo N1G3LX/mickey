96df9efc06a4f3980a79e79d312292955967c3a6
Adewale shittu <sheriffhardewale@gmail.com>
2024-03-22 13:13:09 +0100

    revert to old db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,54 +1,45 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// let cached = global.mongoose;
+let cached = global.mongoose;
 
-// if (!cached) {
-//   cached = global.mongoose = { conn: null, promise: null };
-// }
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGODB_URI, {
+  if (cached.conn) {
+    console.log("🚀 Using cached connection");
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    console.log('connection sucessful')
-  } catch (error) {
-    throw new Error('Error connecting to db')
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then((mongoose) => {
+        console.log("✅ New connection established");
+        return mongoose;
+      })
+      .catch((error) => {
+        console.error("❌ Connection to database failed");
+        throw error;
+      });
   }
-  // if (cached.conn) {
-  //   console.log("🚀 Using cached connection");
-  //   return cached.conn;
-  // }
-  // if (!cached.promise) {
-  //   const opts = {
-  //     bufferCommands: false,
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   };
 
-  //   cached.promise = mongoose
-  //     .connect(MONGODB_URI, opts)
-  //     .then((mongoose) => {
-  //       console.log("✅ New connection established");
-  //       return mongoose;
-  //     })
-  //     .catch((error) => {
-  //       console.error("❌ Connection to database failed");
-  //       throw error;
-  //     });
-  // }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
 
-  // try {
-  //   cached.conn = await cached.promise;
-  // } catch (e) {
-  //   cached.promise = null;
-  //   throw e;
-  // }
-
-  // return cached.conn;
+  return cached.conn;
 };
 
 export default connectDB;
